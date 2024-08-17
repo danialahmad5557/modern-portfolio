b2554361be2957335f9d23252628f172537a0c78
DANIAL AHMAD <109754828+danialahmad5557@users.noreply.github.com>
2024-08-17 12:52:31 +0500

    Add files via upload

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Danial Ahmad</span> a passionate website developer with one year of professional experience in a software house. My expertise lies in crafting responsive and visually appealing websites that deliver great user experiences. I'm constantly learning and improving my skills to stay at the forefront of web development trends. Let's create something amazing together!"
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
